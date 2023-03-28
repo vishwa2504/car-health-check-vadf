c776189c1722cb8471df8f5977fa8bb397ef6c00
vishwa2504 <109200494+vishwa2504@users.noreply.github.com>
2023-03-28 14:46:37 +0000

    vcan_test_1

--- app/tests/integration/integration_test.py
+++ app/tests/integration/integration_test.py
@@ -13,14 +13,45 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import json
-
 import pytest
 from sdv.test.inttesthelper import IntTestHelper
 from sdv.test.mqtt_util import MqttClient
+import can
+import time
 
 # GET_SPEED_REQUEST_TOPIC = "sampleapp/getSpeed"
 # GET_SPEED_RESPONSE_TOPIC = "sampleapp/getSpeed/response"
 
+@pytest.mark.asyncio
+async def test_can_bus():
+    bus1 = can.interface.Bus('test', bustype='virtual')
+    bus2 = can.interface.Bus('test', bustype='virtual')
+
+    msg1 = can.Message(arbitration_id=0x3B2, data=[1])
+    bus1.send(msg1)
+    msg2 = bus2.recv()
+
+    #assert msg1 == msg2
+    assert msg1.arbitration_id != msg2.arbitration_id
+    assert msg1.data == msg2.data
+    assert msg1.timestamp != msg2.timestamp
+    
+@pytest.mark.asyncio
+async def test_socket_can_bus():
+    bustype = 'virtual'
+    channel = 'vcan0' # can0 on non-virtual physical bus
+
+    bus = can.interface.Bus(channel=channel, bustype=bustype, bitrate=500000)
+    for i in range(6000):
+        #driver door open, key in run, engine off
+        msg = can.Message(
+            is_extended_id=False, 
+            arbitration_id=0x3B2, 
+            data=[0x77, 0x80, 0x06, 0x80, 0xD9, 0x06, 0x00, 0x00])
+        print(msg)
+        bus.send(msg)
+        time.sleep(0.01) # send every 10 ms
+        
 
 @pytest.mark.asyncio
 async def test_get_current_speed():
